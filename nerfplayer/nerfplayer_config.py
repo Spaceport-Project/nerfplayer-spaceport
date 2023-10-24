23740a5bb4dc6bc8b4ef2518eed4b8484c02b67a
FarukUgurcali <f.ugurcali@gmail.com>
2023-10-24 10:11:39 +0300

    Multicamera dataset support added to the nerfplayer based on nerfstudio/dataparser/nerfplayer_multicam_dataparser.py, can be used with models nerfacto-nerfplayer-multicam or nerfacto-ngp-multicam

--- nerfplayer/nerfplayer_config.py
+++ nerfplayer/nerfplayer_config.py
@@ -10,6 +10,7 @@ from nerfstudio.data.datamanagers.base_datamanager import (
     VanillaDataManagerConfig,
 )
 from nerfstudio.data.dataparsers.dycheck_dataparser import DycheckDataParserConfig
+from nerfstudio.data.dataparsers.nerfplayer_multicam_dataparser import NerfplayerMulticamDataParserConfig
 from nerfstudio.data.datasets.depth_dataset import DepthDataset
 from nerfstudio.engine.optimizers import AdamOptimizerConfig
 from nerfstudio.engine.trainer import TrainerConfig
@@ -56,6 +57,41 @@ nerfplayer_nerfacto = MethodSpecification(
     description="NeRFPlayer with nerfacto backbone.",
 )
 
+nerfplayer_nerfacto_multicam = MethodSpecification(
+    config=TrainerConfig(
+        method_name="nerfplayer-nerfacto-multicam",
+        steps_per_eval_batch=500,
+        steps_per_save=2000,
+        max_num_iterations=30000,
+        mixed_precision=True,
+        pipeline=VanillaPipelineConfig(
+            datamanager=VanillaDataManagerConfig(
+                _target=VanillaDataManager[DepthDataset],
+                dataparser=NerfplayerMulticamDataParserConfig(),
+                train_num_rays_per_batch=4096,
+                eval_num_rays_per_batch=4096,
+                camera_optimizer=CameraOptimizerConfig(
+                    mode="SO3xR3", optimizer=AdamOptimizerConfig(lr=6e-4, eps=1e-8, weight_decay=1e-2)
+                ),
+            ),
+            model=NerfplayerNerfactoModelConfig(eval_num_rays_per_chunk=1 << 15),
+        ),
+        optimizers={
+            "proposal_networks": {
+                "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+                "scheduler": None,
+            },
+            "fields": {
+                "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+                "scheduler": None,
+            },
+        },
+        viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
+        vis="viewer",
+    ),
+    description="NeRFPlayer with nerfacto backbone.",
+)
+
 nerfplayer_ngp = MethodSpecification(
     config=TrainerConfig(
         method_name="nerfplayer-ngp",
@@ -67,6 +103,40 @@ nerfplayer_ngp = MethodSpecification(
             datamanager=VanillaDataManagerConfig(
                 _target=VanillaDataManager[DepthDataset],
                 dataparser=DycheckDataParserConfig(),
+                train_num_rays_per_batch=8192,
+            ),
+            model=NerfplayerNGPModelConfig(
+                eval_num_rays_per_chunk=8192,
+                grid_levels=1,
+                alpha_thre=0.0,
+                render_step_size=0.001,
+                disable_scene_contraction=True,
+                near_plane=0.01,
+            ),
+        ),
+        optimizers={
+            "fields": {
+                "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+                "scheduler": None,
+            }
+        },
+        viewer=ViewerConfig(num_rays_per_chunk=64000),
+        vis="viewer",
+    ),
+    description="NeRFPlayer with InstantNGP backbone.",
+)
+
+nerfplayer_ngp_multicam = MethodSpecification(
+    config=TrainerConfig(
+        method_name="nerfplayer-ngp-multicam",
+        steps_per_eval_batch=500,
+        steps_per_save=2000,
+        max_num_iterations=30000,
+        mixed_precision=True,
+        pipeline=DynamicBatchPipelineConfig(
+            datamanager=VanillaDataManagerConfig(
+                _target=VanillaDataManager[DepthDataset],
+                dataparser=NerfplayerMulticamDataParserConfig(),
                 train_num_rays_per_batch=8192,
             ),
             model=NerfplayerNGPModelConfig(
